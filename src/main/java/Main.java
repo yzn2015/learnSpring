8969a4dc7129590c6f5d406a688acdc31c34abb7
yzn <20156660@s.hlju.edu.cn>
2019-08-22 00:20:58 +0800

    xmlBeanFactory learn code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("wdwhiojdiow");
+        ClassPathResource resource = new ClassPathResource("app.xml");
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(resource);
     }
 }
